78f1ef44dbb4aea63bfc75359f6e04b4310ce084
leolezner <twols@me.com>
2012-05-28 19:01:53 +0200

    First working uncommented version

---
+++ dummy.py
@@ -0,0 +1,8 @@
+import sys
+
+def main():
+    
+    print("Hello from dummy!", sys.argv)
+
+    
+if __name__ == "__main__": main()
---
+++ pylaunch.py
@@ -0,0 +1,142 @@
+__version__ = "0.0.1"
+__all__ = []
+
+from tkinter import *
+from tkinter import filedialog
+import subprocess
+
+class PyLaunch():
+    def __init__(self, title="PyLaunch"):
+        self._fields = []
+        self._title = title
+        self._window_height = 500
+        self._window_width = 700
+        
+    def field(self, field_type, field_id, field_title, field_validator=None):
+        self._fields.append({"type": field_type, "id": field_id, "title": field_title, "validator": field_validator})
+
+    def launch(self, callback):
+        self._callback = callback
+        self._root = Tk()
+        self._root.title(self._title)
+        self._main_frame = Frame()
+        self._main_frame.pack(expand=YES, fill=BOTH)
+        self._create_window()
+        
+        self._fields_frame = Frame(self._main_frame)
+        self._fields_frame.pack(expand=NO, fill=X)
+        
+        for field in self._fields:
+            self.create_field_widget(field)
+        
+        self._create_result_widget()
+        self._create_buttons()
+        self._root.mainloop()
+        
+    def create_field_widget(self, field):
+        frame = Frame(self._fields_frame)
+        
+        if field["type"] == "text":
+            input_widget = Text(frame, height=1, width=70, relief=SUNKEN, bd=1)
+            element = input_widget
+        elif field["type"] == "file" or field["type"] == "folder":
+            element = Frame(frame)
+            input_widget = Text(element, height=1, width=70, relief=SUNKEN, bd=1)
+            Button(element, text="Browse " + field["type"] + "s", command=lambda: self._on_button_select_file(input_widget, field["type"])).pack(side=RIGHT)
+            input_widget.pack(side=BOTTOM, expand=YES, fill=X)
+            element.pack(side=BOTTOM)
+        
+        input_widget.bind("<Tab>", self.on_field_tab)
+        input_widget.bind("<Return>", self.on_field_enter)
+        field["widget"] = input_widget
+        Label(frame, text=field["title"], justify=LEFT, anchor=W).pack(expand=YES, fill=X)
+        element.pack(side=BOTTOM, expand=YES, fill=X)
+        frame.pack(side=TOP, expand=YES, fill=X)
+        
+    def _on_button_select_file(self, text_field, dialog_type):
+        if dialog_type == "file":
+            filename = filedialog.askopenfilename().strip()
+        else:
+            filename = filedialog.askdirectory().strip()
+        
+        if filename == "":
+            return
+        text_field.delete(1.0, END)
+        text_field.insert(1.0, filename)
+        
+    def on_field_tab(self, element):
+        return "break"
+
+    def on_field_enter(self, element):
+        self._on_button_launch()
+        return "break"
+        
+    def _create_buttons(self):
+        frame = Frame(self._main_frame)
+        launch_button = Button(frame, text="Launch", command=self._on_button_launch)
+        launch_button.pack(side=LEFT)
+        Button(frame, text="Launch and Exit", command=self._on_button_launch_and_exit).pack(side=LEFT)
+        Button(frame, text="Clear", command=self._on_button_clear).pack(side=LEFT)
+        Button(frame, text="Exit", command=self._on_button_exit).pack(side=LEFT)
+        frame.pack(side=BOTTOM)
+        
+    def _create_result_widget(self):
+        frame = Frame(self._main_frame)
+        Label(frame, text="Results", justify=LEFT, anchor=W).pack()
+        self._result_widget = Text(frame, height=10, width=70, relief=SUNKEN, bd=1)
+        self._result_widget.pack(expand=YES, fill=BOTH)
+        frame.pack(side=BOTTOM, expand=YES, fill=BOTH)
+
+    def _create_window(self):
+        left = (self._root.winfo_screenwidth() / 2) - (self._window_width / 2)
+        top = (self._root.winfo_screenheight() / 2) - (self._window_height / 2)
+        self._root.geometry('{}x{}+{}+{}'.format(self._window_width, self._window_height, int(left), int(top)))
+    
+    def _generate_command_line(self):
+        params = []
+        
+        for field in self._fields:
+            params.append([field["id"], field["widget"].get(1.0, END)])
+        
+        return self._callback(params)
+    
+    def _on_button_launch(self):
+        self._on_button_clear()
+        self._result_widget.insert(END, "Please wait...")
+        params = self._generate_command_line()
+        try:
+            cmd = [sys.executable]
+            cmd.extend(params)
+            result = subprocess.check_output(cmd).decode("utf-8")
+        except OSError as err:
+            result = err
+        
+        self._on_button_clear()
+        self._result_widget.insert(END, "{}\n".format(result))
+        self._result_widget.see(END)
+        
+    def _on_button_launch_and_exit(self):
+        self._on_button_launch()
+        self._on_button_exit()
+        
+    def _on_button_clear(self):
+        self._result_widget.delete(1.0, END)
+        
+    def _on_button_exit(self):
+        self._root.destroy()
+
+def pylaunch_callback(params):
+    return ["dummy.py"] + ([p[1].strip() for p in params])
+
+def main():
+    pl = PyLaunch(title="My Script Launcher")
+    
+    pl.field(field_type="text", field_id="vorname", field_title="Dein Vorname")
+    pl.field(field_type="text", field_id="nachname", field_title="Dein Nachname")
+    pl.field(field_type="file", field_id="inputfile1", field_title="Die CSV-Tabelle mit den Motoren")
+    pl.field(field_type="file", field_id="inputfile2", field_title="Die CSV-Tabelle mit den Umrichtern")
+    pl.field(field_type="folder", field_id="folder", field_title="Ordner mit den Fotos")
+    
+    pl.launch(pylaunch_callback)
+    
+if __name__ == "__main__": main()
